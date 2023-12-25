a06ab04dae638a877e907b751f0eab354f18f1a5
George <flannellad@mail.ru>
2023-12-25 22:03:56 +0400

    excercises

--- app.js
+++ app.js
@@ -10,3 +10,11 @@ console.log(city);
 let hobby = 'Chess';
 console.log(hobby);
 
+let admin;
+let name2 = "John";
+admin = name2;
+alert(admin);
+
+let planetName = "Earth";
+console.log(planetName);
+let currentUser; 
