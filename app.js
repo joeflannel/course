80ee9f123b1b7a1b98dfe1a401512bb7ac27502b
George <flannellad@mail.ru>
2023-12-25 21:46:49 +0400

    new

--- app.js
+++ app.js
@@ -1,1 +1,12 @@
-console.log("Hello world!");
+let name = 'Georgii'
+console.log(name);
+
+let age = 20;
+console.log(age);
+
+let city = 'Yerevan';
+console.log(city);
+
+let hobby = 'Chess';
+console.log(hobby);
+
